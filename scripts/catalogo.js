d8ff37e4815ac722745a450450c5a107c5441742
Enzo D'Andrea <enzojdandrea@gmail.com>
2022-10-24 15:48:44 -0300

    agregada funciones de vasos y platos

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -1,6 +1,6 @@
+let total=0,totalUnEvento=0;
 function inicial(){
-    let cantEventos;
-    let diferentes;
+    let cantEventos,iguales;
    alert("Comencemos");
    do{
     cantEventos=parseInt(prompt("Cuantos eventos desea diseñar(Solo se Permite hasta 10 eventos) "));
@@ -8,28 +8,89 @@ function inicial(){
    
    do{
     if(cantEventos!=1){
-        diferentes=prompt("Los eventos seran todos iguales SI/NO");
+        iguales=prompt("Los eventos seran todos iguales SI/NO");
     }else{
-        diferentes="no";
+        iguales="no";
     }
-   }while(diferentes!='si' && diferentes!='no');
+   }while(iguales!='si' && iguales!='no');
 
-   if(diferentes=="si"){
+   if(iguales=="si"){
     varioseventos(1);
+    total=total*cantEventos;
+    if(total==totalUnEvento){
+        alert("El precio del evento es de :  "+total);
+       }else{
+        alert("El precio de todos los eventos es de :  "+total+"\nEl total de cada evento es de :  "+totalUnEvento);
+    }
    }else{
     varioseventos(cantEventos);
-   }
-   
+    alert("El precio del evento es de :  "+total);
+   }  
 }
 
 function varioseventos(cantidad){
-    for(let i=1;i<=cantidad;i++){
-        selcVaso();
-        selcPlato();
-        selcMantel();
-        selcMesa();
-        selcUbi();
+    total=0;
+    totalUnEvento=0;
+    alert("cantidad :  "+cantidad);
+ for(let i=1;i<=cantidad;i++){
+     total=total+selcVaso();
+     total=total+selcPlato();
+    //  total=total+selcMantel();
+    //  total=total+selcMesa();
+    //  total=total+selcUbi();
+     if(i==1){
+         totalUnEvento=total;
+     }
+    }    
+}
+
+function selcVaso(){
+    let selec,valor;
+    do{
+        selec=prompt("1- Vaso 1 - Precio = $10\n2- Vaso 2 - Precio = $20\n3- Vaso 3 - Precio = $30\n4- Vaso 4 - Precio = $40");    
+    }while(selec!=1&&selec!=2&&selec!=3&&selec!=4)
+    switch (selec){
+        case "1":
+            valor = 10;
+            break;
+        case "2":
+            valor = 20;
+            break;
+        case "3":
+            valor = 30;
+            break;
+        case "4":
+            valor = 40;
+            break;
+        default:
+            valor = 0;
+            break;
     }
+    return valor;
+}
+function selcPlato(){
+    let selec,valor;
+    do{
+        selec=prompt("1- Plato 1 - Precio = $10\n2- Plato 2 - Precio = $20\n3- Plato 3 - Precio = $30\n4- Plato 4 - Precio = $40");    
+    }while(selec!=1&&selec!=2&&selec!=3&&selec!=4)
+    switch (selec){
+        case "1":
+            valor = 10;
+            break;
+        case "2":
+            valor = 20;
+            break;
+        case "3":
+            valor = 30;
+            break;
+        case "4":
+            valor = 40;
+            break;
+        default:
+            valor = 0;
+            break;
+    }
+    return valor;
 }
 
 
